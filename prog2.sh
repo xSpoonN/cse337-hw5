e038e0fb3c779530e13467ebf6515829448f85e7
Kevin Tao <kevin.tao@stonybrook.edu>
2022-11-28 20:53:21 -0500

    prog 2 done

--- prog2.sh
+++ prog2.sh
@@ -4,4 +4,19 @@
 WRONG_ARGS_MSG="data file or output file missing"
 FILE_NOT_FOUND_MSG="file not found"
 
-# FILL ME
+if test $# != 2; then echo $WRONG_ARGS_MSG; exit 0; fi
+if test -f $1; then
+    rm -f $2
+    touch $2
+    max="0"
+    for i in $(awk -F ':|;|,' '{print NF}' $1); do
+        if test $i -gt $max; then max=$i; fi
+    done;
+    for (( i = 1; i <= $max; i++ )); do
+        awk -F ':|;|,' -v col=$i '{if ($col != "") sum+=$col; else sum+=0;} END{printf "Col " col " : " sum "\n"; }' $1 >> $2
+    done;
+else 
+    name="$(basename -- $1)"
+    echo "${name} ${FILE_NOT_FOUND_MSG}"; 
+fi
+exit 0
